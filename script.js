bc3ea2a271499e7c1d629987fbf39a43b8a1295f
perinov <novruzovaperi2@gmail.com>
2023-09-30 22:54:03 +0400

    created weather api

---
+++ script.js
@@ -0,0 +1,32 @@
+const city = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+const icon = document.querySelector('.icon');
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    Weather(city.value);
+})
+
+async function Weather(cityName) {
+
+    const apiKey = "a25c64308d1540a429c6e500948ed49c";
+    const baseUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}`;
+
+    const response = await fetch(baseUrl + `&appid=${apiKey}`);
+
+    let data = await response.json();
+
+    console.log(data);
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = 'Temp-' + Math.round(data.main.temp) + '°C';
+    document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
+    document.querySelector('.wind').innerHTML = data.wind.deg;
+    document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h';
+    data.weather.forEach(item => {
+        icon.setAttribute("src", "http://openweathermap.org/img/w/" + item.icon + ".png");
+        icon.style.display = "block";
+    });
+
+
+}
+
